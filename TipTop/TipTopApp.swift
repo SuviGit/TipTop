f6b1cf06c70341be04e8c7d8527ad7697c8c2055
Suvidha <suvidha.nakhawa@gmail.com>
2021-07-04 15:28:58 -0400

    Initial Commit

---
+++ TipTop/TipTopApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipTopApp.swift
+//  TipTop
+//
+//  Created by Suvidha on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct TipTopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
